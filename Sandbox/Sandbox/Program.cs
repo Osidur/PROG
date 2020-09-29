8cbdf48c2366dc4de37c5750b22f830d9d83d8d0
Osidur <hugo.pollack@elev.ga.ntig.se>
2020-09-29 12:51:53 +0200

    Update Program.cs

--- Sandbox/Sandbox/Program.cs
+++ Sandbox/Sandbox/Program.cs
@@ -14,6 +14,11 @@ namespace Sandbox
             switch (user_choice)
             {
                 case 1:
+                    Console.WriteLine("Name:");
+                    String name = Console.ReadLine();
+                    Console.WriteLine("Product id:");
+                    String productId = Console.ReadLine();
+                    Customer user = new Customer(name, productId);
                     break;
                 case 2:
                     break;
