11e9ab594b2f912f8b00eba81b10cc5e86563f82
Osidur <hugo.pollack@elev.ga.ntig.se>
2020-10-01 10:21:26 +0200

    gfh

--- Sandbox/Sandbox/Program.cs
+++ Sandbox/Sandbox/Program.cs
@@ -1,43 +1,8 @@
 ﻿using System;
 
-namespace Sandbox
+public class Class1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            while(true)
-            {
-                Start();
-                Choices();
-            }
-        }
-    void Start() 
-    {
-        Console.WriteLine("Add customer: 1");
-        Console.WriteLine("Add product: 2");
-        Console.WriteLine("Exit program: 3");
-        int user_choice = Convert.ToInt32(Console.ReadLine());
-        }
-    void Choices() {
-        switch (user_choice)
-            {
-                case 1:
-                    Console.WriteLine("Name:");
-                    String name = Console.ReadLine();
-                    Console.WriteLine("Product id:");
-                    String productId = Console.ReadLine();
-                    User customer = new User(name, productId);
-                    break;
-                case 2:
-                    Console.WriteLine("Id:");
-                    String id = Console.ReadLine();
-                    User customer = new User(id);
-                    break;
-                case 3:
-                    Application.Exit;
-                    break;
-        }
-    }
+	public Class1()
+	{
+	}
 }
-    }
