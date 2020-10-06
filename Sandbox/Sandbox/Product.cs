debaf9b478571ca7fa8c2a3577949c94f589c97d
Osidur <hugo.pollack@elev.ga.ntig.se>
2020-10-06 11:48:47 +0200

    va

--- Sandbox/Sandbox/Product.cs
+++ Sandbox/Sandbox/Product.cs
@@ -1,11 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Sandbox
-{
-    class Product()
-    {
-        int _productId;
-    }
-}
+﻿
--- Sandbox/Sandbox/Program.cs
+++ Sandbox/Sandbox/Program.cs
@@ -1,17 +1,1 @@
-﻿using Sandbox;
-using System;
-
-public class Program
-{
-	public Main(string args)
-	{
-		/* 
-		*En huvudklass som kör programmet.
-		*En klass som representerar en Kund.
-		*En klass som representerar en Produkt.
-		*/
-
-		createUser();
-
-	}
-}
+﻿va
